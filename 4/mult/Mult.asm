c9bf7fd0c293efa846355cec06973d0dce51f79f
alexajau <ajauregui@cocc.edu>
2025-11-10 22:30:56 -0800

    Add mult.asm

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -1,10 +1,29 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/4/Mult.asm
+@2 //load r2
+M=0 //set R2 to 0
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-// The algorithm is based on repetitive addition.
+@1 //load r1
+D=M //get value of r1
+@INFINITE_LOOP 
+D;JEQ //if r1 = 0 jump to end
 
-//// Replace this comment with your code.
+@0 //load r0
+D=M //get value of r0
+@INFINITE_LOOP
+D;JEQ //if r0 = 0 jump to end
+
+(LOOP)
+	@0 //load r0
+	D=M //load the value of r0
+	@2 //load R2
+	M=D+M //add r0 to r2
+
+	@1 //load r1
+	M=M-1 //r1 = r1 - 1
+	
+	D=M //load updated r1
+	@LOOP
+	D;JGT //if r1>0 then loop
+
+(INFINITE_LOOP)
+@INFINITE_LOOP
+0;JMP //infinite loop
