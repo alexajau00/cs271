93c513cb5fadc9caedb90e5fb676bde083aa54ba
alexajau <ajauregui@cocc.edu>
2025-11-10 22:37:51 -0800

    Add Fill.asm

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -1,11 +1,45 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/4/Fill.asm
+(LOOP)
+@KBD //load address of keyboard 
+D=M //Set whether the keyboard is pressed
+@WHITE
+D;JEQ //jump to white if no key is pressed
+@BLACK
+D;JGT //jump to black if any key is pressed
+@LOOP
+0;JMP
 
-// Runs an infinite loop that listens to the keyboard input. 
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
-// the screen should be cleared.
+(WHITE)
+@color //var to store the curr color
+M=0 //set color to white
+@FILL 
+0;JMP // jump to add the color
 
-//// Replace this comment with your code.
+(BLACK)
+@color //var to store the curr color
+M=-1 //set color to black
+@FILL
+0;JMP //jump to add the color
+
+(FILL)
+@SCREEN //load address of the screen
+D=A //load address of screen
+@address //var to keep track of the pixel
+M=D //address equals the screen
+
+(FILL_SCREEN)
+@color 
+D=M //load the color
+@address
+A=M //load the address
+M=D //draw the color on the pixel
+
+@address
+M=M+1 //move to next pixel
+D=M //update address
+@KBD
+D=A-D //Keyboard - Address
+@FILL_SCREEN
+D;JGT //jump if address < keyboard
+@LOOP
+0;JMP // or else loop
+
