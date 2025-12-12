94340cb04af82f3c1bef15d4390da69f80f956e8
alexajau <ajauregui@cocc.edu>
2025-12-11 17:44:14 -0800

    Added Cploration 9

---
+++ cplorations/c09/parser.c
@@ -0,0 +1,189 @@
+/****************************************
+ * C-ploration 6 for CS 271
+ * 
+ * [NAME] Alexa Jauregui
+ * [TERM] FALL 2025
+ * 
+ ****************************************/
+#include "parser.h"
+#include "symtable.h"
+#include "error.h"
+
+
+/* Function: strip
+ * 
+ * remove whitespace and comments from a line
+ *
+ * s: the char* string to strip
+ *
+ * returns: the stripped char* string
+ */
+char *strip(char *s){	
+
+	// your code here
+	char s_new[strlen(s) + 1];
+	int i = 0;
+	for(char *s2 = s; *s2; s2++) {
+		if(*s2 == '/' && *(s2+1) == '/'){
+			break;
+		} else if (!isspace(*s2)){
+			s_new[i++] = *s2;
+		}
+	}
+	s_new[i] = '\0';
+	strcpy(s, s_new);
+    return s;	
+}
+
+/* Function: parse
+ * -------------
+ * iterate each line in the file and strip whitespace and comments. 
+ *
+ * file: pointer to FILE to parse
+ *
+ * returns: nothing
+ */
+void parse(FILE * file){
+	
+	// your code here
+	char line[MAX_LINE_LENGTH] = {0};
+	char label[MAX_LABEL_LENGTH] = {0};
+
+	hack_addr instr_addr = 0;
+	unsigned int line_num = 0;
+	unsigned int instr_num = 0;
+	char inst_type;
+	instruction instr;
+	add_predefined_symbols();
+
+	while(fgets(line, sizeof(line), file)){
+		line_num++;
+		if(instr_num > MAX_INSTRUCTIONS) {
+			exit_program(EXIT_TOO_MANY_INSTRUCTIONS, MAX_INSTRUCTIONS + 1);
+		}
+		strip(line);
+		if(!*line){
+			continue;
+		}
+		if(is_Atype(line)) {
+			if (!parse_A_instruction(line, &instr.instr.a)){
+    			exit_program(EXIT_INVALID_A_INSTR, line_num, line);
+ 			}
+ 			instr.itype = Atype;
+			inst_type = 'A';
+			instr_addr++;
+		} else if(is_label(line)){
+			inst_type = 'L';
+			extract_label(line, label);
+			if(!isalpha((unsigned char)label[0])){
+				exit_program(EXIT_INVALID_LABEL, line_num, label);
+			} else if (symtable_find(label) != NULL){
+				exit_program(EXIT_SYMBOL_ALREADY_EXISTS, line_num, label);
+			} else {
+				symtable_insert(label, instr_num);
+				continue;
+			}
+		} else {
+			inst_type = 'C';
+			instr_addr++;
+		}
+		//printf("%u: %c  %s\n", instr_num, inst_type, line);
+		instr_num++;
+	}
+}
+
+
+/* Function: is_Atype
+ * -------------
+ * Determine whether the line is an address instruction depending on if it starts with @
+ *
+ * line: the line of an assembly file
+ *
+ * returns: boolean
+ */
+bool is_Atype(const char *line){
+	if(line[0] == '@'){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+/* Function: is_label
+ * -------------
+ * Determine whether the line is a label depending on if it starts with '(' and ends with ')'
+ *
+ * line: the line of an assembly file
+ *
+ * returns: boolean
+ */
+bool is_label(const char *line){
+	size_t len = strlen(line);
+	if(line[0] == '(' && line[len - 1] == ')'){
+		return true;
+	} else{
+		return false;
+	}
+}
+
+
+/* Function: is_Ctype
+ * -------------
+ * Determine whether the line is a C-type instruction depending on if it's not a label or address instruction
+ *
+ * line: the line of an assembly file
+ *
+ * returns: boolean
+ */
+bool is_Ctype(const char *line){
+	if(is_label(line) == false && is_Atype(line) == false){
+		return true;
+	} else{
+		return false;
+	}
+}
+
+
+/* Function: extract_label
+ * -------------
+ * Extracts the name of the label and puts it into the variable label
+ *
+ * line: the line of an assembly file
+ * label: the label in the line
+ *
+ * returns: label
+ */
+char *extract_label(const char *line, char* label){
+	strcpy(label, line + 1);
+	label[strlen(label) - 1] = '\0';
+	return label;
+}
+
+void add_predefined_symbols(){
+	for(int i = 0; i < NUM_PREDEFINED_SYMBOLS; i++){
+		predefined_symbol pre_sym = predefined_symbols[i];
+		symtable_insert(pre_sym.name, pre_sym.address);
+	}
+}
+
+
+bool parse_A_instruction(const char *line, a_instruction *instr){
+	size_t len = strlen(line);
+	char *s = malloc(len); 
+	strcpy(s, line + 1);
+	char *s_end = NULL;
+	long result = strtol(s, &s_end, 10);
+	if(s == s_end){
+		instr->label = (char *)malloc(strlen(s) + 1);
+		strcpy(instr->label, s);
+		instr->is_addr = false;
+		return true;
+	} else if(*s_end != 0){
+		return false;
+	}else{
+		instr->address = result;
+		instr->is_addr = true;
+	}
+	return true;
+}
