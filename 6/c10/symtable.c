940cccb1d178ca9d34c46e8ac72d5eaf379e1692
alexajau <ajauregui@cocc.edu>
2025-12-11 18:28:33 -0800

    Move cploration 10 to project 6 file

---
+++ 6/c10/symtable.c
@@ -0,0 +1,64 @@
+#include "symtable.h"
+
+Symbol* hashArray[SYMBOL_TABLE_SIZE];
+
+
+int hash(char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while((c = *str++))
+        hash = ((hash << 5) + hash) + c; /*hash * 33 + c*/
+    return hash % SYMBOL_TABLE_SIZE;
+}
+
+
+void symtable_insert(char *name, hack_addr addr){
+    Symbol *item = (Symbol*) malloc(sizeof(Symbol));
+    item -> addr = addr;
+    item -> name = strdup(name);
+
+    int hashIndex = hash(name);
+
+    while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->name != NULL) {
+        ++hashIndex;
+        hashIndex %= SYMBOL_TABLE_SIZE;
+    }
+    hashArray[hashIndex] = item;
+}
+
+
+Symbol *symtable_find(char *name){
+    int hashIndex = hash(name);
+
+    while(hashArray[hashIndex] != NULL){
+        if (strcmp(hashArray[hashIndex]->name, name) == 0)
+            return hashArray[hashIndex];
+        
+        ++hashIndex;
+
+        hashIndex %= SYMBOL_TABLE_SIZE;
+    }
+    return NULL;
+}
+
+
+void symtable_display_table(void){
+    int i = 0;
+    for(i=0; i<SYMBOL_TABLE_SIZE; i++) {
+        if(hashArray[i] != NULL)
+            printf(" (%s%d)", hashArray[i]->name, hashArray[i]->addr);
+        else
+            printf(" ~~ ");
+    }
+    printf("\n");
+}
+
+void symtable_print_labels(void){
+    int i = 0;
+    for(i = 0; i<SYMBOL_TABLE_SIZE; i++) {
+        if(hashArray[i] != NULL) {
+            printf("{%s,%d}\n", hashArray[i]->name, hashArray[i]->addr);
+        }
+    }
+}
